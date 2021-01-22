40f60d5ea3a9eb06e0600aed878cacbab5915914
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-01-22 17:30:57 +0200

    Some cleanup in the result getting functions

--- src/GetResults.ts
+++ src/GetResults.ts
@@ -6,38 +6,32 @@ export interface Result {
 	seats: number
 }
 
-function getPercentages(parties: Party[], nullVotes: number): [ number[], number ] {
+function getVoters(parties: Party[], nullVotes: number) {
 	var voters = nullVotes;
 	for (const party of parties) {
 		voters += party.votes;
 	}
 
-	var percentages = [];
-	for (const party of parties) {
-		percentages.push(party.votes*100/voters);
-	}
-
-	return [ percentages, nullVotes*100/voters ];
+	return [ nullVotes*100/voters, voters ];
 }
 
-export function dHontResults(parties: Party[], seats: number, treshold: number, nullVotes: number): [ Result[], number ] {
-	// If we further calculate the percentages of an empty list of parties, the program crashes
+function highestAveragesCalculator(parties: Party[], seats: number, treshold: number, nullVotes: number, quotinentFn: (votes: number, seats: number) => number): [ Result[], number ] {
 	if (parties.length === 0) {
 		return [ [], 0];
 	}
 
 	var results: Result[] = [];
-	var quotinents = [];
-	var [ percentages, nullVotesPercentage ] = getPercentages(parties, nullVotes);
+	var quotinents: number[] = [];
+	var [ nullVotesPercentage, voters ] = getVoters(parties, nullVotes);
 	
 	// Generating the results array; setting the coeficient as the number of votes for each party
 	for (var i = 0; i < parties.length; i++) {
 		results.push({
 			name: parties[i].name,
-			percentage: percentages[i],
+			percentage: parties[i].votes*100/voters,
 			seats: 0
 		});
-		quotinents.push(parties[i].votes);
+		quotinents.push(quotinentFn(parties[i].votes, 0));
 	}
 
 	// Getting the $seats best quotinents; generating new ones after adding seats to their parties
@@ -45,14 +39,15 @@ export function dHontResults(parties: Party[], seats: number, treshold: number, 
 		var maxQuotinent = 0;
 		var currentWinner = -1;
 		for (var i = 0; i < parties.length; i++) {
-			if (maxQuotinent < quotinents[i] && percentages[i] >= treshold) {
+			if (maxQuotinent < quotinents[i] && results[i].percentage >= treshold) {
 				maxQuotinent = quotinents[i];
 				currentWinner = i;
 			}
 		}
+		// If no party passes the treshold, the current winner stays -1, and no one will get a seat
 		if (currentWinner > -1) {
 			results[currentWinner].seats++;
-			quotinents[currentWinner] = parties[currentWinner].votes / (1 + results[currentWinner].seats);
+			quotinents[currentWinner] = quotinentFn(parties[currentWinner].votes, results[currentWinner].seats);
 		}
 	}
 
@@ -60,48 +55,13 @@ export function dHontResults(parties: Party[], seats: number, treshold: number, 
 	return [ results.sort((a, b) => {
 		return b.percentage - a.percentage;
 	}), nullVotesPercentage ];
+}
+export function dHontResults(parties: Party[], seats: number, treshold: number, nullVotes: number): [ Result[], number ] {
+	return highestAveragesCalculator(parties, seats, treshold, nullVotes, (votes: number, seats: number) => votes / (seats + 1));
 }
 
 export function websterResults(parties: Party[], seats: number, treshold: number, nullVotes: number): [ Result[], number ] {
-	// If we further calculate the percentages of an empty list of parties, the program crashes
-	if (parties.length === 0) {
-		return [ [], 0];
-	}
-
-	var results: Result[] = [];
-	var quotinents = [];
-	var [ percentages, nullVotesPercentage ] = getPercentages(parties, nullVotes);
-	
-	// Generating the results array; setting the coeficient as the number of votes for each party
-	for (var i = 0; i < parties.length; i++) {
-		results.push({
-			name: parties[i].name,
-			percentage: percentages[i],
-			seats: 0
-		});
-		quotinents.push(parties[i].votes);
-	}
-
-	// Getting the $seats best quotinents; generating new ones after adding seats to their parties
-	for (var j = 0; j < seats; j++) {
-		var maxQuotinent = 0;
-		var currentWinner = -1;
-		for (var i = 0; i < parties.length; i++) {
-			if (maxQuotinent < quotinents[i] && percentages[i] >= treshold) {
-				maxQuotinent = quotinents[i];
-				currentWinner = i;
-			}
-		}
-		if (currentWinner > -1) {
-			results[currentWinner].seats++;
-			quotinents[currentWinner] = parties[currentWinner].votes / (1 + results[currentWinner].seats*2);
-		}
-	}
-
-	// Sort the results decreasingly by percentage and return them with the null votes percentage
-	return [ results.sort((a, b) => {
-		return b.percentage - a.percentage;
-	}), nullVotesPercentage ];
+	return highestAveragesCalculator(parties, seats, treshold, nullVotes, (votes: number, seats: number) => votes / (2*seats + 1));
 }
 
 export const votingMethods = {
